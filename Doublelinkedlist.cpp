f5a9aa975a690edaa4b41b0e38afd66620b64004
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-16 08:36:51 +0700

    Implement addNode method for inserting nodes into the doubly linked list

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -16,8 +16,45 @@ class DoubleLinkedList
         Node *START;
 
     public:
-    DoubleLinkedList()
+        DoubleLinkedList()
     {
         START = NULL;
     }
-}
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEneter the roll number of the student: ";
+        cin >> nim;
+
+        //step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        //step 2: Assign value to the data field
+        newNode->noMhs = nim;
+
+        //step 3: Insert at beginning if liist is empty or nim is smallest
+        if (START == NULL || nim <<= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            //step 4: newNode.next = start
+            newNode->next = START;
+
+            //step 5: START.prev = newNode (if START exists)
+            if (START != NULL)
+                START->prev = newNode;
+
+            //step 6: newNode.prev = NULL
+            newNode->prev = NULL;
+
+            //step 7: START = newNode
+            START = newNode;
+            return;
+        }
+        
+    }
+};
