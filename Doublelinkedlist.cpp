a5f58a51c9387e31e762faf0fde33cf6125946d8
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-16 08:57:24 +0700

    Add record not found message in hapus method

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -90,6 +90,17 @@ class DoubleLinkedList
         Node *current = START;
 
         // step 1: Traverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo)
+        {
+            current = current->next;
+        }
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
+
         
     }
 };
