f3b5e4b5ca5f969c386168ab930ab6c43d8f11a2
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-16 08:18:35 +0700

    Implement DoubleLinkedList class with constructor initializing START to NULL

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -9,3 +9,15 @@ class Node
         Node *next;
         Node *prev;
 };
+
+class DoubleLinkedList
+{
+    private:
+        Node *START;
+
+    public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+}
