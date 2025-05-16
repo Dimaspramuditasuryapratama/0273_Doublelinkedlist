a213e330a5242f3940564b7fc683e5e0643e1c34
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-16 09:40:06 +0700

    Add searchData method to find a student record by roll number

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -175,5 +175,20 @@ class DoubleLinkedList
         }
     }
 
-    
+    void searchData()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        int rollNo;
+        cout << "\nEnter the roll number to search: ";
+        cin >> rollNo;
+
+        Node *current = START;
+
+        
+    }
 };
